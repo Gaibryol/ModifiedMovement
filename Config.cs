a92f4e58aeaf2396354d67e71def8d7c67ba6a31
Gaibryol <gaibryol@gmail.com>
2024-02-11 19:29:32 -0700

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,111 @@
+﻿using BepInEx.Configuration;
+using System;
+using Unity.Collections;
+using Unity.Netcode;
+
+namespace ModifiedMovement
+{
+	public class Config : SyncedInstance<Config>
+	{
+		public static ConfigEntry<float> maxStaminaMultiplier;
+		public static ConfigEntry<float> staminaUsageMultiplier;
+		public static ConfigEntry<float> staminaRegenMultiplierWalking;
+		public static ConfigEntry<float> staminaRegenMultiplierStationary;
+		public static ConfigEntry<float> sprintSpeed;
+
+		public Config(ConfigFile cfg)
+		{
+			InitInstance(this);
+
+			maxStaminaMultiplier = cfg.Bind(
+				"Movespeed",
+				"MaxStaminaMultiplier",
+				1f,
+				"Multiplier for maximum stamina (Default = 1)"
+			);
+
+			staminaUsageMultiplier = cfg.Bind(
+				"Movespeed",
+				"SprintUsageMultiplier",
+				1f,
+				"Multiplier for the rate stamina gets used when sprinting (Default = 1)"
+			);
+
+			staminaRegenMultiplierWalking = cfg.Bind(
+				"Movespeed",
+				"StaminaRegenMultiplierWalking",
+				1f,
+				"Multiplier for the rate stamina regenerates when walking (Default = 1)"
+			);
+
+			staminaRegenMultiplierStationary = cfg.Bind(
+				"Movespeed",
+				"StaminaRegenMultiplierStationary",
+				1f,
+				"Multiplier for the rate stamina regenerates when stationary (Default = 1)"
+			);
+
+			sprintSpeed = cfg.Bind(
+				"Movespeed",
+				"SprintSpeed",
+				2.25f,
+				"Maximum sprinting speed (Default = 2.25)"
+			);
+		}
+
+		public static void RequestSync()
+		{
+			if (!IsClient) return;
+
+			using FastBufferWriter stream = new(IntSize, Allocator.Temp);
+			MessageManager.SendNamedMessage("ModName_OnRequestConfigSync", 0uL, stream);
+		}
+
+		public static void OnRequestSync(ulong clientId, FastBufferReader _)
+		{
+			if (!IsHost) return;
+
+			//Plugin.Logger.LogInfo($"Config sync request received from client: {clientId}");
+
+			byte[] array = SerializeToBytes(Instance);
+			int value = array.Length;
+
+			using FastBufferWriter stream = new(value + IntSize, Allocator.Temp);
+
+			try
+			{
+				stream.WriteValueSafe(in value, default);
+				stream.WriteBytesSafe(array);
+
+				MessageManager.SendNamedMessage("ModName_OnReceiveConfigSync", clientId, stream);
+			}
+			catch (Exception e)
+			{
+				//Plugin.Logger.LogInfo($"Error occurred syncing config with client: {clientId}\n{e}");
+			}
+		}
+
+		public static void OnReceiveSync(ulong _, FastBufferReader reader)
+		{
+			if (!reader.TryBeginRead(IntSize))
+			{
+				//Plugin.Logger.LogError("Config sync error: Could not begin reading buffer.");
+				return;
+			}
+
+			reader.ReadValueSafe(out int val, default);
+			if (!reader.TryBeginRead(val))
+			{
+				//Plugin.Logger.LogError("Config sync error: Host could not sync.");
+				return;
+			}
+
+			byte[] data = new byte[val];
+			reader.ReadBytesSafe(ref data, val);
+
+			SyncInstance(data);
+
+			//Plugin.Logger.LogInfo("Successfully synced config with host.");
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using ModifiedMovement;
+using ModifiedMovement.Patches;
+using UnityEngine;
+
+namespace ModifiedMovement
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+		private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+		public static Config MyConfig { get; internal set; }
+
+		private static Plugin Instance;
+
+        private void Awake()
+        {
+			if (Instance == null)
+			{
+				Instance = this;
+			}
+
+			MyConfig = new(base.Config);
+
+			harmony.PatchAll(typeof(Plugin));
+			harmony.PatchAll(typeof(PlayerControllerBPatch));
+			harmony.PatchAll(typeof(Config));
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+        }
+    }
+}
+
+namespace ModifiedMovement.Patches
+{
+	[HarmonyPatch(typeof(PlayerControllerB))]
+	internal class PlayerControllerBPatch
+	{
+		[HarmonyPatch("Update")]
+		[HarmonyPostfix]
+		static void ModifiedMovementPatch(ref float ___sprintMeter, ref bool ___isSprinting, ref bool ___isWalking, ref float ___sprintTime, ref float ___carryWeight, ref float ___sprintMultiplier)
+		{
+			if (___isSprinting)
+			{
+				___sprintMeter = Mathf.Clamp(___sprintMeter - ((Time.deltaTime / ___sprintTime * ___carryWeight) * Config.staminaUsageMultiplier.Value), 0f, Config.maxStaminaMultiplier.Value);
+				___sprintMultiplier = Mathf.Lerp(___sprintMultiplier, Config.sprintSpeed.Value, Time.deltaTime);
+			}
+			else
+			{
+				___sprintMultiplier = Mathf.Lerp(___sprintMultiplier, 1f, Time.deltaTime);
+
+				if (___isWalking)
+				{
+					___sprintMeter = Mathf.Clamp(___sprintMeter + ((Time.deltaTime / (___sprintTime + 9f)) * Config.staminaRegenMultiplierWalking.Value), 0f, Config.maxStaminaMultiplier.Value);
+				}
+				else
+				{
+					___sprintMeter = Mathf.Clamp(___sprintMeter + ((Time.deltaTime / (___sprintTime + 4f)) * Config.staminaRegenMultiplierStationary.Value), 0f, Config.maxStaminaMultiplier.Value);
+				}
+			}
+		}
+
+		[HarmonyPatch("ConnectClientToPlayerObject")]
+		[HarmonyPostfix]
+		public static void InitializeLocalPlayer()
+		{
+			if (Config.IsHost)
+			{
+				Config.MessageManager.RegisterNamedMessageHandler("ModName_OnRequestConfigSync", Config.OnRequestSync);
+				Config.Synced = true;
+
+				return;
+			}
+
+			Config.Synced = false;
+			Config.MessageManager.RegisterNamedMessageHandler("ModName_OnReceiveConfigSync", Config.OnReceiveSync);
+			Config.RequestSync();
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(GameNetworkManager), "StartDisconnect")]
+		public static void PlayerLeave()
+		{
+			Config.RevertSync();
+		}
+	}
+}
